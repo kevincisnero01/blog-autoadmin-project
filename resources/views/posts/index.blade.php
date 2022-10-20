d03fd5d584d2e47e0368b53ee2ba2f1c3665c2c5
kevincisnero01 <7884578+kevincisnero01@users.noreply.github.com>
2022-10-20 19:45:34 -0400

    Post List IV
    -Responsive Layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
 <div class="container">
-    <div class="grid grid-cols-3 gap-6 mt-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
         @foreach ($posts as $post)
             <article 
-                class="w-full h-80 bg-cover bg-center @if($loop->first) col-span-2 @endif" 
+                class="w-full h-80 bg-cover bg-center @if($loop->first) sm:col-span-2 @endif" 
                 style="background-image: url({{ Storage::url($post->image->url) }})"
             >
                 <div class=" w-full h-full px-8 flex flex-col justify-center">
