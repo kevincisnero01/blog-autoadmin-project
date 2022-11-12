c64eeeaa8912ecd68899bda350afc4696cf2acd9
kevincisnero01 <7884578+kevincisnero01@users.noreply.github.com>
2022-11-12 11:50:49 -0400

    Category List
    -controller
    -view
    -style

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,46 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Listado de Categorias</h1>
+@stop
+
+@section('content')
+    
+    <div class="card">
+        <div class="card-header">
+            <a href="{{ route('admin.categories.create')}}" class="btn btn-secondary btn-lg">Agregar Categoria</a>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped align-middle">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    @foreach ($categories as $category)
+                    <tr>
+                        <td>{{ $category->id }}</td>
+                        <td>{{ $category->name }}</td>
+                        <td width="200px" class="text-center">
+                            <a href="{{ route('admin.categories.edit', $category->id)}}" class="btn btn-primary btn-xs">Editar</a>
+                            <form action="{{ route('admin.categories.destroy', $category->id)}}" method="post" class="d-inline">
+                                @csrf
+                                @method('delete')
+
+                                <button type="submit" class="btn btn-danger btn-xs">Eliminar</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>    
+    </div>
+
+@stop
+
