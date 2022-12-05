5f4ff6ba38897efcd13f9106a276398b91d9671a
kevincisnero01 <7884578+kevincisnero01@users.noreply.github.com>
2022-12-05 19:50:56 -0400

    Tag Delete

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -61,10 +61,10 @@ class TagController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  $id
+     * @param  $tag
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($tag)
     {
 
     }
@@ -72,7 +72,7 @@ class TagController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  $id
+     * @param  $tag
      * @return \Illuminate\Http\Response
      */
     public function edit(Tag $tag)
@@ -96,10 +96,10 @@ class TagController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  int  $tag
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Tag $tag)
+    public function update(Request $request,Tag $tag)
     {
         $data = $request->validate([
             'name' => 'required',
@@ -115,11 +115,14 @@ class TagController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int  $tag
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Tag $tag)
     {
-        //
+        $tag->delete();
+
+        return redirect()->route('admin.tags.index')->with('status','Eliminación Exitosa');
+
     }
 }
