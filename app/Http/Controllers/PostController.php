e52f606424232c115964eb1e5bec859c372c6110
kevincisnero01 <7884578+kevincisnero01@users.noreply.github.com>
2023-01-08 17:53:08 -0400

    Policy Post  Published
    -The user cannot view a post with a draft status in the public area.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,6 +17,8 @@ class PostController extends Controller
 
     public function show(Post $post)
     {   
+        $this->authorize('published',$post);
+
         $related = Post::where('category_id',$post->category_id)
                         ->where('status',2)
                         ->where('id','!=',$post->id)
--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -10,10 +10,18 @@ class PostPolicy
 {
     use HandlesAuthorization;
 
+    public function published(?User $user, Post $post)
+    {
+        if($post->status == 2){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public function author(User $user, Post $post)
     {
-        if($user->id == $post->user_id)
-        {
+        if($user->id == $post->user_id){
             return true;
         }else{
             return false;
