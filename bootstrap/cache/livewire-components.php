2321f5724122b339cfbec47d28bde2efe15c0a8a
kevincisnero01 <7884578+kevincisnero01@users.noreply.github.com>
2023-01-10 20:15:29 -0400

    Fix Boostrap cache

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,5 @@
+<?php return array (
+  'admin.post-index' => 'App\\Http\\Livewire\\Admin\\PostIndex',
+  'admin.users-index' => 'App\\Http\\Livewire\\Admin\\UsersIndex',
+  'navigation' => 'App\\Http\\Livewire\\Navigation',
+);
