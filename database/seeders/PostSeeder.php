c50a2460c2bcb2aef9caa349a59caa07c9753b90
kevincisnero01 <7884578+kevincisnero01@users.noreply.github.com>
2023-01-16 22:10:45 -0400

    Seeder Post Real data
    -Real data for presentation

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -15,7 +15,31 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        $posts = Post::factory(5)->create();
+        //Post::truncate();
+
+        $csvFile = fopen(base_path("database/data/post.csv"), "r");
+        $firstline = true;
+
+        while (($data = fgetcsv($csvFile, 2000, ";")) !== FALSE) {
+            if (!$firstline) {
+                Post::create([
+                    "id" => utf8_encode($data['0']),
+                    "name" => utf8_encode($data['1']),
+                    "slug" => utf8_encode($data['2']),
+                    "extract" => utf8_encode($data['3']),
+                    "body" => utf8_encode($data['4']),
+                    "status" => utf8_encode($data['5']),
+                    "user_id" => utf8_encode($data['6']),
+                    "category_id" => utf8_encode($data['7'])
+                ]);    
+            }
+            $firstline = false;
+        }
+        
+        fclose($csvFile);
+
+
+        $posts = Post::all();
 
         foreach($posts as $post)
         {
