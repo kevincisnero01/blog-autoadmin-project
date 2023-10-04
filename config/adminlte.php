c4de9cea1775c89c16a5cd005f8dd6a9e85db8b4
kevincisnero01 <kevingabriel18@gmail.com>
2023-10-03 21:32:05 -0400

    Mejorar apariencia agregandole estilos de marca

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,12 +63,12 @@ return [
     |
     */
 
-    'logo' => 'El blog de <b>Kevin</b>',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => 'BlogAutoAdmi',
+    'logo_img' => 'branch/logo-full.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'Admin Logo',
+    'logo_img_alt' => 'BlogAutoAdmi',
 
     /*
     |--------------------------------------------------------------------------
@@ -309,7 +309,7 @@ return [
             'can'  => 'manage-blog',
         ],
         [
-            'text' => 'Dashboard',
+            'text' => 'Escritorio',
             'route' => 'admin.home',
             'icon' => 'fas fa-tachometer-alt fa-fw',
             'can' => 'admin.home'
